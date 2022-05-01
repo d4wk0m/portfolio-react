26ad997631c29b02318e4897f321caf6f693f0fe
d4wk0m <96721232+d4wk0m@users.noreply.github.com>
2022-05-01 21:08:50 +0200

    Finished landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../css/landing.scss'
 
 function landing() {
   return (
-    <div>landing</div>
+    <div className="hero">
+      <div className="hero-text">
+        <h1>Dawid Komęza</h1>
+        <div className="hero-caption">
+            <h2>Junior </h2><span className="hero-accent">Front-end</span><h2> Developer</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
